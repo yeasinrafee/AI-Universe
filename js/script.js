3133c970f4f18452126dee0bd36e36a95f429fa4
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-03-03 07:15:22 +0600

    geting data by fetch API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 
+const getData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.tools);
+}
+getData();
